dfb636987af0bda31934fc9992fe11acfa59c478
RobinCSI <126656636+RobinCSI@users.noreply.github.com>
2023-03-30 15:58:46 +0530

    Lucky number changes only when a user guesses it correctly

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, {useState, useRef} from "react";
 import styles from "./App.module.css";
 
+
 export default function App() {
 
   const [val, setVal]=useState()
+  const [lucky, setLucky]=useState(luckyNum())
   const count=useRef(0)
-  const lucky=100
+
+  function luckyNum(){
+    const lucky=Math.floor(Math.random()*100)
+    return (lucky)
+  }
 
   const handleChange=(event)=>{
     setVal(event.target.value)
@@ -13,6 +19,7 @@ export default function App() {
 
   const handleClick=()=>{
     count.current+=1
+    // console.log(lucky)
     if(val<lucky){
       alert("You guessed a smaller number")
       setVal('')
@@ -25,11 +32,13 @@ export default function App() {
       alert("Congratulations! You guessed the right number in "+count.current+ " attempts")
       setVal('')
       count.current=0
+      setLucky(luckyNum()) //Lucky number gets changed only when a user guesses it correctly
     }
   }
   return (
     <div className={styles.container}>
-      <input value ={val} onChange={handleChange} placeholder="Guess lucky number ..." />
+      <div>Guess a number between 0 and 100 to try your luck : </div>
+      <input value ={val} onChange={handleChange} placeholder="Guess lucky number .." />
       <button className={styles.btn} onClick={handleClick}>Match Number</button>
     </div>
   );
