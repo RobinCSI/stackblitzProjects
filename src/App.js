db0c16a7e0cdd0cf37fa1cdea3ee8e4a277b84bc
RobinCSI <126656636+RobinCSI@users.noreply.github.com>
2023-04-10 21:43:19 +0530

    1 file modified, 1 added

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+
+import InputNumTable from './MultiplicationTable/InputNumTable';
+import AppRandomTable from './MultiplicationTable/AppRandomTable';
+
+import GreetingChange from './GreetingChange/GreetingChange';
+import EventLog from './EventLog/EventLog';
+
+export default function App() {
+  return (
+    <Fragment>
+      {/* <InputNumTable /> */}
+      {/* <AppRandomTable /> */}
+      <GreetingChange />
+      {/* <EventLog /> */}
+    </Fragment>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import InputNumTable from './MultiplicationTable/InputNumTable';
-import AppRandomTable from './MultiplicationTable/AppRandomTable';
-
-
-import GreetingChange from './GreetingChange/GreetingChange';
-import EventLog from './EventLog/EventLog';
-
-
+import App from './App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    {/* <InputNumTable /> */}
-    {/* <AppRandomTable /> */}
-    {/* <GreetingChange /> */}
-    
-
-    <EventLog />
+    <App />
   </StrictMode>
 );
