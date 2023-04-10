d6dc228cfc4ef7ac982ef275f51ff8f75a9a21a8
RobinCSI <126656636+RobinCSI@users.noreply.github.com>
2023-04-10 21:45:03 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Fragment } from 'react';
 
 import InputNumTable from './MultiplicationTable/InputNumTable';
 import AppRandomTable from './MultiplicationTable/AppRandomTable';
-
 import GreetingChange from './GreetingChange/GreetingChange';
 import EventLog from './EventLog/EventLog';
 
