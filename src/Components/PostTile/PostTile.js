457d125a0e67115b0858c8ce532371f3785d3072
RobinCSI <126656636+RobinCSI@users.noreply.github.com>
2023-04-14 01:25:33 +0530

    2 files modified, 1 added

--- src/Components/PostTile/PostTile.js
+++ src/Components/PostTile/PostTile.js
@@ -1,8 +1,26 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState} from 'react'
+// import {uuid} from 'uuid'
+import { v4 as uuid } from 'uuid'
+
+import styles from 'PostTile.module.css'
+
 
 export default function PostTile(){
 
-  fetch("https://jsonplaceholder.typicode.com/posts").then(response=>response.json()).then(data=>console.log(data))
+  const [postList, setPostList]=useState([])
+
+  async function fetchPostData(){
+    const postsData=await fetch("https://jsonplaceholder.typicode.com/posts").then(response=>response.json())
+    setPostList(postsData)
+    
+  }
+  // console.log(postList)
+
+  function deletePost(deleteId){
+
+    setPostList(postList.filter(post=>post.id!=deleteId))
+
+  }
 
 
 
@@ -11,12 +29,19 @@ export default function PostTile(){
     <Fragment>
 
     <h1>Post List</h1>
+    <button onClick={fetchPostData}>Fetch Posts Data</button>
     <table>
+      <thead>
         <tr>
-          <th>Element</th>
-          <th>Time</th>
-          <th>Event Name</th>
+          <th>ID</th>
+          <th>User ID</th>
+          <th>Title</th>
+          <th>Body</th>
         </tr>
+        </thead>
+        <tbody>
+          {postList.map(post=><tr key={uuid()}><td>{post.id}</td><td>{post.userId}</td><td>{post.title}</td><td>{post.body}</td><td onClick={()=>deletePost(post.id)} className={styles.delete}>❌</td></tr>)}
+        </tbody>
     </table>
 
     </Fragment>
