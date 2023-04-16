30bfe10d57c4f17d4d4cdcdca660193af777c57d
RobinCSI <126656636+RobinCSI@users.noreply.github.com>
2023-04-16 14:01:33 +0530

    1 file modified

--- src/Components/ProductInput/ProductInput.js
+++ src/Components/ProductInput/ProductInput.js
@@ -4,41 +4,58 @@ import DropdownList from "react-widgets/DropdownList"
 import styles from './ProductInput.module.css'
 
 export default function ProductInput(){
-
+  // const productsArr=[{product: "Sugar", productID: 1, productQty:0}, {product: "Oats", productID: 2, productQty:0}, {product: "Coffee", productID: 3, productQty:0}, {product: "Snacks", productID: 4, productQty:0}]
   const [qty, setQty]=useState(0)
+  const products=useRef([{product: "Sugar", productID: 1, productQty:0}, {product: "Oats", productID: 2, productQty:0}, {product: "Coffee", productID: 3, productQty:0}, {product: "Snacks", productID: 4, productQty:0}])
 
-  const productsArr=[{product: "Sugar", productID: 1, productQty:0}, {product: "Oats", productID: 2, productQty:0}, {product: "Coffee", productID: 3, productQty:0}, {product: "Snacks", productID: 4, productQty:0}]
+  function handleSubmit(e){
+    e.preventDefault()
+    console.log(e.target)
+    
+  }
 
-  const products=useRef(productsArr)
+  }
 
 
   function inputQty(){
 
-    products.map(prodt=>(prodt.product==value? prodt.productQty+=qty : prodt))
+    products.current.map(prodt=>(prodt.product==value? setQty(prodt.productQty+qty) : prodt))
 
   }
 
-  function alertWhenSelected(event){
+  // function alertWhenSelected(){
 
-    console.log(event.target.value)
-  }
+  //   alert('selected!')
+  // }
 
 
   return(
 
     <div className={styles.productInput}>
+      <form onSubmit={handleSubmit}>
       <div className={styles.selectProduct}>
-        <label>Select Product</label>
-        <DropdownList
+      
+        <label>Select Product
+        <select name="selectedProduct" defaultValue="select">
+        {products.current.map(prodt=><option value={prodt}>{prodt.product}</option>)}
+          {/* <option value="Sugar">Grapefruit</option>
+          <option value="lime">Lime</option>
+          <option value="coconut">Coconut</option>
+          <option value="mango">Mango</option> */}
+        </select>
+        </label>
+        {/* <DropdownList
     defaultValue="Select" dataKey="productID" textField="product" onSelect={alertWhenSelected}
-    data={products}
-  />
+    data={products.current}
+  /> */}
       </div>
       <div className={styles.selectQty}>
-        <label>Select quantity</label>
+        <label>Select quantity
         <input type="number" onChange={inputQty} value={qty} />
+        </label>
       </div>
-      <button>Add more products</button>
+      <button type="submit">Add more products</button>
+      </form>
     </div>
   )
 }
