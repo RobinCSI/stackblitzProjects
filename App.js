6bdbd991c919d2650703cee1cfd976c2fe8c5cf2
RobinCSI <126656636+RobinCSI@users.noreply.github.com>
2023-04-25 02:33:57 +0530

    2 files added

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+import Counter from './features/counter/Counter'
+
+export default function App() {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+}
